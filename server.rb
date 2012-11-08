94239e271d12467b6d53378b47c77c13b4a9ccbf
Aman Kapur <amankapur91@gmail.com>
2012-11-08 16:01:41 -0500

    arduino comm started

--- server.rb
+++ server.rb
@@ -1,11 +1,31 @@
+require 'rubygems'
 require 'sinatra'
+require 'serialport'
+require 'thread'
+
+
+
+
+sp = SerialPort.new "/dev/ttyACM0", 9600
+sp.write('hi')
+puts sp.read()
+
+
+
+
 
 Tilt.register Tilt::ERBTemplate, 'html.erb'
-
 def herb(template, options={}, locals={})
   render "html.erb", template, options, locals
 end
 
 get '/' do 
 	herb :index
+end
+
+
+post '/sync' do 
+	pos = params[:pos]
+	len = params[:len]
+	
 end
