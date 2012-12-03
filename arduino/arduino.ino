9bcfe897fb057f2e458e2469e76ec4c4a8f19315
Aman Kapur <amankapur91@gmail.com>
2012-12-03 15:55:03 -0500

    yt links work

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -16,36 +16,38 @@ void setup()
   Serial.begin(9600);          //Open serial communication
   Serial.println("pause 0.12 -");
 }
- 
+
 void loop()
 {
   // send state and pos to server
-//  dtostrf (pos, '4', '2', pos2);
-//  Serial.println(state + ' ' + pos2 + " -");
-  
+  //  dtostrf (pos, '4', '2', pos2);
+  //  Serial.println(state + ' ' + pos2 + " -");
+
   // incoming message from server
-//  if (Serial.available() >0){
-    message = "Synch 0.21 -"; 
-    handleMessage(message);
-//}
-  
+  //  if (Serial.available() >0){
+  message = "Synch 0.21 -"; 
+  handleMessage(message);
+  //}
+
   // play/pause button press
   if (buttonPress(buttonPin)) {
-   if (state == "pause") {
+    if (state == "pause") {
       state == "play";
-   } 
-   else {state == "pause";}
+    } 
+    else {
+      state == "pause";
+    }
   }
-  
+
   // slider position change
   if(userMovedSlider()){
-   int new_pos = getPos(motor);
-   }
-  
+    int new_pos = getPos(motor);
+  }
+
 }
 
 // get position from mesg in string and float
-// this is JANKETY
+// this is JANKETY but it works for now
 void handleMessage(String mesg) {
   int i;
   char *pch;
@@ -65,41 +67,46 @@ void handleMessage(String mesg) {
   }
 }
 // Gets position from slider
+// VERY IMPORTANT FUNCTION!!!
+
 int getPos(int motor){
   return 0;
 }
 
 // checks if slider has been moved by user
 boolean userMovedSlider(){
-  
- return false; 
+
+  return false; 
 }
 
 
 // was button pressed?
 boolean buttonPress(int buttonPin){
- if (digitalRead(buttonPin) == HIGH) 
-   {return true ;}
- return false;
-  
+  if (digitalRead(buttonPin) == HIGH) 
+  {
+    return true ;
+  }
+  return false;
+
 }
 
 // waits for all incoming bytes till char c
 // return string of data recevied
 String chkSer(char c){
   String content = "";
-    char character;
-    while(content.indexOf(c) <0){
-       // Serial.println('content');
-        if(Serial.available() >0){
-          character = Serial.read();
-          content.concat(character);
-          //Serial.println(content);
-     
-      }
+  char character;
+  while(content.indexOf(c) <0){
+    // Serial.println('content');
+    if(Serial.available() >0){
+      character = Serial.read();
+      content.concat(character);
+      //Serial.println(content);
+
     }
-    return content;
+  }
+  return content;
 }
+
 
 
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,31 @@
 $(document).on('ready', function(){
 	var dev_key =  "AI39si6p8JyCYDoSBE6Fcv16d7Xykw_trX4LVPHooYk9Y5uaY3VlveaH3XYMJO-El2gcQ1J8woIsa1-lGzyBMtmD6uCmu1FJ_w"
 	$('#search').submit(function() {
-		$("#yt_vids").html('');
-    // get all the inputs into an array.
-    var inputs = $('#search :input');
-
-  	var query = inputs.serializeArray()[0].value;
-  	
-  	
-  	var url = "https://gdata.youtube.com/feeds/api/videos?q=" + encodeURI(query) + "&key=" + dev_key;
+	        $("#yt_vids").html('');
+                // get all the inputs into an array.
+                var inputs = $('#search :input');
+  	        var query = inputs.serializeArray()[0].value;
+          	var url = "https://gdata.youtube.com/feeds/api/videos?q=" + encodeURI(query) + "&key=" + dev_key;
 		$.get(url, function(data){
 
 			var entries = $(data).find('entry');
-  		for (var vid =0; vid < 10; vid++){
-  			var entry = $(entries[vid]);
-				var thumbnail = entry.find("thumbnail")[1];
-				var thumbnail_url = thumbnail.getAttribute('url');
-				var temp = entry.find("content")[1];
-  			var vid_url = temp.getAttribute('url');
-  			var html = '<div class="row"> <img src = ' + thumbnail_url + '> <h2> Title </h2> </div>';
-  			$("#yt_vids").append(html);
-  			
-  		}
+                        console.log(data);
+                        window.d = data;
+          		for (var vid =0; vid < 10; vid++){
+  	        		var entry = $(entries[vid]);
+	        		var thumbnail = entry.find("thumbnail")[1];
+	        		var thumbnail_url = thumbnail.getAttribute('url');
+	        		var temp = entry.find("content")[1];
+  	        		var vid_url = temp.getAttribute('url');
+                                var title = entry.find('title').text();
+          			var html = '<div class="row"> <a href=' + vid_url + '><img src = ' + thumbnail_url + '></a> <h2>'+ title+' </h2> </div>';
+  	        		$("#yt_vids").append(html);
+  		        	
+  		        }
   			
 		});
 
   	
-		return false;
-		});
+	return false;
+       });
 });
