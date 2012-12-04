8139642caf629e225d4e6ac2da6e0c297e355e00
Matt Belland <mattbelland@gmail.com>
2012-12-04 00:30:19 -0500

    stashing tuesday work

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,6 @@
 var pos, lastpos = 0; //These need to be in an object with structure
+var serverLoop;
+var playerObj = null;
 
 $(document).on('ready', function(){
     var dev_key =  "AI39si6p8JyCYDoSBE6Fcv16d7Xykw_trX4LVPHooYk9Y5uaY3VlveaH3XYMJO-El2gcQ1J8woIsa1-lGzyBMtmD6uCmu1FJ_w"
@@ -18,22 +20,51 @@ $(document).on('ready', function(){
                 var thumbnail_url = thumbnail.getAttribute('url');
                 var temp = entry.find("content")[1];
                 var vid_url = temp.getAttribute('url');
+                var vid_id = youtube_id_from_url(vid_url);
                 var title = entry.find('title').text();
-                var html = '<div class="row"><a href="#"><img src = ' + thumbnail_url + ' onClick="loadPlayer(\'' + vid_url + '\')"></a> <h2>'+ title + ' </h2> </div>';
+                var html = '<div class="row"><a href="#"><img src = ' + thumbnail_url + ' onClick="loadPlayer(\'' + vid_id + '\')"></a> <h2>'+ title + ' </h2> </div>';
                 $("#yt_vids").append(html);
             }
         });
     return false;
     });
-    window.setInterval(function(){
-        getState();
-        postSync();
-        postLength();
-    }, 100);
 });
 
-function loadPlayer(video) {
-    console.log(video);
+window.onkeydown = fullScreenPlayer;
+//window.addEventListener('onkeydown', function (keyEvent) {
+function fullScreenPlayer(keyEvent) {
+    console.log('a');
+    var keyCode = ('which' in event) ? event.which : event.keyCode;
+    console.log(keyCode);
+    if (keyCode == 70 && playerObj) {
+        //playerObj.requestFullScreen();
+        fullScreenApi.requestFullScreen(playerObj);
+    }
+}
+
+function GetChar (event){
+            var keyCode = ('which' in event) ? event.which : event.keyCode;
+            alert ("The Unicode key code is: " + keyCode);
+        }
+
+function loadPlayer(vid_id) {
+    // Lets Flash from another domain call JavaScript
+    var params = { allowScriptAccess: "always" };
+    // The element id of the Flash embed
+    var atts = { id: "ytPlayer" };
+    swfobject.embedSWF("http://www.youtube.com/v/" + vid_id + "?version=3&enablejsapi=1&playerapiid=player1", 
+                       "videoDiv", "480", "295", "9", null, null, params, atts);
+    playerObj = $("object#ytPlayer");
+
+    postLength();
+
+    window.clearInterval(serverLoop); //This really needs to be placed where the video is closed, not here.
+    serverLoop = window.setInterval(function(){
+        getState();
+        postSync();
+        //postLength();
+    }, 100);
+    $("videoDiv")
 }
 
 function getState() {
@@ -42,7 +73,7 @@ function getState() {
         url: "/state",
         dataType: "json",
         success: function(data){
-            console.log(data);
+            //console.log(data);
             lastpos = pos;
             pos = data.pos;
             if (Math.abs(pos-lastpos) > 5) {
@@ -69,4 +100,15 @@ function postLength() {
         dataType: "json",
         data: {"len":100}
     });
+}
+
+//NOT http://stackoverflow.com/questions/6556559/youtube-api-extract-video-id
+//http://stackoverflow.com/questions/3452546/javascript-regex-how-to-get-youtube-video-id-from-url
+function youtube_id_from_url(url) {
+    var video_id = url.split('v/')[1];
+    var ampersandPosition = video_id.indexOf('?');
+    if(ampersandPosition != -1) {
+        video_id = video_id.substring(0, ampersandPosition);
+    }
+    return video_id;
 }
